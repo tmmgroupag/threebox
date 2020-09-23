b508fbc341bf5c6570b94f5f797d685d984a2d13
Timo Lösch <github@tlxhost.de>
2020-09-23 23:23:59 +0200

    added svgloader to the exports added new builds

--- exports.js
+++ exports.js
@@ -1,2 +1,5 @@
-window.Threebox = require('./src/Threebox.js'),
-window.THREE = require('./src/three.js')
+window.Threebox = require('./src/Threebox.js');
+window.THREE = require('three');
+window.mapboxgl = require('mapbox-gl');
+window.THREE.SVGLoader = require('three/examples/js/loaders/SVGLoader').SVGLoader;
+
