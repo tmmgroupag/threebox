ae25cedf2a3909647a25a65d3213d25bc50ac1cc
Timo Lösch <github@tlxhost.de>
2020-09-24 09:36:21 +0200

    removed mapboxgl dependency from threebox changed version of three js to 0.118.8 due compatibility

--- exports.js
+++ exports.js
@@ -1,3 +1,2 @@
 window.Threebox = require('./src/Threebox.js');
 window.THREE = require('three');
-window.mapboxgl = require('mapbox-gl');
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 module.exports = exports = {
     Threebox: require('./src/Threebox'),
     THREE: require('three'),
-    mapboxgl: require('mapbox-gl'),
 }
